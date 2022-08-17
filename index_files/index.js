ba5fe597adf6a991269cc931575310c35ad618a2
Luca Borletti <lgborletti@gmail.com>
2022-08-17 16:09:45 -0700

    added logo variations

--- index_files/index.js
+++ index_files/index.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    // const toolbeltW = document.getElementById('drawing-wrapper'); 
     const toolbelt = document.getElementById('toolbelt');
     const toolbeltg = document.getElementById('toolbeltg');
     const allElements = toolbeltg.children;
@@ -9,42 +10,57 @@ window.onload = function() {
     const blueprint = document.getElementById('g_bp');
     const hammer = document.getElementById('g_hammer');
     const wrench = document.getElementById('g_wrench');
+    const phone = document.getElementById('g_phone');
 
     const schoolPath = document.getElementById('school_path');
     const workPath = document.getElementById('work_path');
     const projectsPath = document.getElementById('projects_path');
+    const contactPath = document.getElementById('contact_path');
     const projectsText = document.getElementById('projects_text');
     const schoolText = document.getElementById('school_text');
     const workText = document.getElementById('work_text');
+    const contactText = document.getElementById('contact_text');
     const gProjects = document.getElementById('g_projects');
     const gSchool = document.getElementById('g_school');
     const gWork = document.getElementById('g_work');
+    const gContact = document.getElementById('g_contact');
 
-    var schoolOn = false;
+    const schoolContainer = document.getElementById('school-container');
+    const workContainer = document.getElementById('work-container');
+    const projectsContainer = document.getElementById('projects-container');
+    const contactContainer = document.getElementById('contact-container');
+
+    var mode = "main"; // "main" || "school" || "work" || "projects" || "contact"
+    // either have a null variable or a bunch of booleans
 
     function returnTools() {
-        pencil1.classList.remove("pencil_1_active");
-        pencil2.classList.remove("pencil_2_active");
-        blueprint.classList.remove("blueprint_active");
-        hammer.classList.remove("hammer_active");
-        wrench.classList.remove("wrench_active");
-        
-        schoolPath.classList.remove("school_path_active");
-        schoolPath.classList.add("school_path_inactive");
+            pencil1.classList.remove("pencil_1_active");
+            pencil2.classList.remove("pencil_2_active");
+            blueprint.classList.remove("blueprint_active");
+            hammer.classList.remove("hammer_active");
+            wrench.classList.remove("wrench_active");
+            phone.classList.remove("phone_active");
+            
+            schoolPath.classList.remove("school_path_active");
+            schoolPath.classList.add("school_path_inactive");
+    
+            workPath.classList.remove("work_path_active");
+            workPath.classList.add("work_path_inactive");
+    
+            projectsPath.classList.remove("projects_path_active");
+            projectsPath.classList.add("projects_path_inactive");
 
-        workPath.classList.remove("work_path_active");
-        workPath.classList.add("work_path_inactive");
-
-        projectsPath.classList.remove("projects_path_active");
-        projectsPath.classList.add("projects_path_inactive");
-        
-        projectsText.classList.add("projects_text_inactive");
-        workText.classList.add("work_text_inactive");
-        schoolText.classList.add("school_text_inactive");
+            contactPath.classList.remove("contact_path_active");
+            contactPath.classList.add("contact_path_inactive");
+            
+            projectsText.classList.add("projects_text_inactive");
+            workText.classList.add("work_text_inactive");
+            schoolText.classList.add("school_text_inactive");
+            contactText.classList.add("contact_text_inactive");
     }
 
 
-    gPencils.addEventListener("mouseover", () => {
+    function hoverSchool() {
         returnTools();
         gSchool.style.display = "unset";
         pencil1.classList.add("pencil_1_active");
@@ -54,9 +70,15 @@ window.onload = function() {
         schoolPath.classList.add("school_path_active");
 
         schoolText.classList.remove("school_text_inactive");
+    }
+
+    gPencils.addEventListener("mouseover", () => {
+        if (mode == "main") {
+            hoverSchool();
+        }
     });
 
-    blueprint.addEventListener("mouseover", () => {
+    function hoverProjects() {
         returnTools();
         gProjects.style.display = "unset";
 
@@ -66,10 +88,15 @@ window.onload = function() {
         projectsPath.classList.add("projects_path_active");
 
         projectsText.classList.remove("projects_text_inactive");
+    }
 
+    blueprint.addEventListener("mouseover", () => {
+        if (mode == "main") {
+            hoverProjects();
+        }
     });
 
-    wrench.addEventListener("mouseover", () => {
+    function hoverWork() {
         returnTools();
         gWork.style.display = "unset";
 
@@ -80,21 +107,38 @@ window.onload = function() {
         workPath.classList.add("work_path_active");
         
         workText.classList.remove("work_text_inactive");
+    }
+
+    wrench.addEventListener("mouseover", () => {
+        if (mode == "main") {
+            hoverWork();
+        }
 
     });
 
     hammer.addEventListener("mouseover", () => {
+        if (mode == "main") {
+            hoverWork();
+        }
+
+    });
+
+    function hoverContact() {
         returnTools();
-        gWork.style.display = "unset";
+        gContact.style.display = "unset";
 
-        hammer.classList.add("hammer_active");
-        wrench.classList.add("wrench_active");
+        phone.classList.add("phone_active");
         
-        workPath.classList.remove("work_path_inactive");
-        workPath.classList.add("work_path_active");
+        contactPath.classList.remove("contact_path_inactive");
+        contactPath.classList.add("contact_path_active");
+        
+        contactText.classList.remove("contact_text_inactive");
+    }
 
-        workText.classList.remove("work_text_inactive");
-
+    phone.addEventListener("mouseover", () => {
+        if (mode == "main") {
+            hoverContact();
+        }
     });
 
     // projectsText.addEventListener("mouseover", () => {
@@ -111,35 +155,152 @@ window.onload = function() {
     
     
     function openSchool() {
+        schoolContainer.classList.remove("container-off");
         for (const elem of allElements) {
             const id = elem.id;
             if (!(id == "g_pencils" || id == "g_school")) {
                 elem.classList.add("invisible");
-                console.log(elem);
-            }
-            else {
-
             }
         }
-        schoolOn = true;
+        mode = "school";
     }
 
     function closeSchool() {
+        schoolContainer.classList.add("container-off");
         for (const elem of allElements) {
             const id = elem.id;
             if (!(id == "g_pencils" || id == "g_school")) {
                 elem.classList.remove("invisible");
             }
-            else {
-
-            }
         }
-        schoolOn = false;
+        mode = "main";
     }
 
+    gPencils.addEventListener("click", () => {
+        if (mode == "school") {
+            closeSchool()
+        }
+        else if (mode == "main") {
+            openSchool()
+        }
+    });
+
+
+
+    function openWork() {
+        workContainer.classList.remove("container-off");
+        for (const elem of allElements) {
+            const id = elem.id;
+            if (!(id == "g_hammer" || id == "g_wrench" || id == "g_work")) {
+                elem.classList.add("invisible");
+            }
+        }
+        mode = "work";
+    }
+
+    function closeWork() {
+        workContainer.classList.add("container-off");
+        for (const elem of allElements) {
+            const id = elem.id;
+            if (!(id == "g_hammer" || id == "g_wrench" || id == "g_work")) {
+                elem.classList.remove("invisible");
+            }
+        }
+        mode = "main";
+    }
+
+    wrench.addEventListener("click", () => {
+        if (mode == "work") {
+            closeWork()
+        }
+        else if (mode == "main") {
+            openWork()
+        }
+    });
+
+    hammer.addEventListener("click", () => {
+        if (mode == "work") {
+            closeWork()
+        }
+        else if (mode == "main") {
+            openWork()
+        }
+    });
+
+
+
+    function openProjects() {
+        projectsContainer.classList.remove("container-off");
+        for (const elem of allElements) {
+            const id = elem.id;
+            if (!(id == "g_bp" || id == "g_projects")) {
+                elem.classList.add("invisible");
+            }
+        }
+        mode = "projects";
+    }
+
+    function closeProjects() {
+        projectsContainer.classList.add("container-off");
+        for (const elem of allElements) {
+            const id = elem.id;
+            if (!(id == "g_bp" || id == "g_projects")) {
+                elem.classList.remove("invisible");
+            }
+        }
+        mode = "main";
+    }
+
+    blueprint.addEventListener("click", () => {
+        if (mode == "projects") {
+            closeProjects()
+        }
+        else if (mode == "main") {
+            openProjects()
+        }
+    });
+    
+    function openContact() {
+        // contactContainer.classList.remove("container-off");
+        for (const elem of allElements) {
+            const id = elem.id;
+            if (!(id == "g_phone" || id == "g_contact")) {
+                elem.classList.add("invisible");
+            }
+            else {
+                elem.style.transform = "translate( -100px, 0px);"
+                // elem.classList.add("move-contact");
+            }
+        }
+        mode = "contact";
+    }
+
+    function closeContact() {
+        // contactContainer.classList.add("container-off");
+        for (const elem of allElements) {
+            const id = elem.id;
+            if (!(id == "g_phone" || id == "g_contact")) {
+                elem.classList.remove("invisible");
+            }
+            else {
+                elem.classList.remove("move-contact");
+            }
+        }
+        mode = "main";
+    }
+
+    phone.addEventListener("click", () => {
+        if (mode == "contact") {
+            closeContact()
+        }
+        else if (mode == "main") {
+            openContact()
+        }
+    });
+
+
     projectsText.addEventListener("click", () => {
-        // one of two choices… we re-reoute to another page
-        // or we erase content and move svg 
+
     });
 
     schoolText.addEventListener("click", () => {
@@ -149,29 +310,10 @@ window.onload = function() {
     workText.addEventListener("click", () => {
 
     });
-
-
-
-
-    gPencils.addEventListener("click", () => {
-        if (schoolOn) {
-            closeSchool()
-        }
-        else {
-            openSchool()
-        }
-    });
-
-    blueprint.addEventListener("click", () => {
-
-    });
-
-    wrench.addEventListener("click", () => {
-
-    });
-
-    hammer.addEventListener("click", () => {
-
-    });
     
+    toolbelt.addEventListener("mouseleave", () => {
+        if (mode == "main") {
+            returnTools();
+        }
+    });
 }
